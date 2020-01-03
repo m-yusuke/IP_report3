c74fa80ab4e8503b857637e4226afac0073edc73
Yusuke Minei <e175703@ie.u-ryukyu.ac.jp>
2020-01-03 16:23:18 +0900

    ex6までできた

--- ex6.py
+++ ex6.py
@@ -2,7 +2,6 @@ import numpy as np
 from PIL import Image, ImageFilter
 import argparse
 import ex5
-import cv2
 
 
 def get_args():
@@ -18,37 +17,21 @@ def get_args():
     return args
 
 def sharpning_filter(array, k=1, radius=1):
-    print(array.shape)
-    #Image.fromarray(array.astype(np.uint8)).show()
     smoothing_array = ex5.gaussian_filter(array, radius=radius)
-    #unsharp_image = cv2.addWeighted(array, 1.5, smoothing_array, -0.5, 0, array)
-    #Image.fromarray(smoothing_array.astype(np.uint8)).show()
-    print(smoothing_array.shape)
-    diff_array = array - smoothing_array
-    #Image.fromarray(diff_array.astype(np.uint8)).show()
-    print(diff_array.shape)
-    result = array + diff_array * k
-    #Image.fromarray(result.astype(np.uint8)).show()
-    print(result.shape)
+    result = float(k + 1) * array - float(k) * smoothing_array
+    result = np.maximum(result, np.zeros(result.shape))
+    result = np.minimum(result, 255 * np.ones(result.shape))
     return result
-    #return unsharp_image
-    #return hoge
-
 
 if __name__ == '__main__':
     args = get_args()
 
     img = Image.open(args.target)
-    #img.filter(ImageFilter.UnsharpMask(radius=1)).show()
     if img.mode != "RGB":
-        print("hoge")
         img = img.convert("RGB")
     img_array = np.array(img)
 
     img_result = sharpning_filter(img_array, args.k, args.radius)
-
-    #diff_array = img_array - img.filter(ImageFilter.BoxBlur(radius=1))
-    #img_result = img_array + diff_array
 
     pil_img = Image.fromarray(img_result.astype(np.uint8))
 
